dafd4d693bd8a3fc5ac55399d109726f799ddd0f
Dmitry Fomin <fomin.dm@gmail.com>
2024-12-29 23:21:29 +0100

    Day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,31 @@
-use std::collections::BTreeMap;
+#[derive(Debug, Clone, Copy)]
+struct Line {
+    fixed: i64,
+    start: i64,
+    end: i64,
+    distance: i64,
+}
+
+impl Line {
+    fn intersection_distance(&self, other: &Line) -> i64 {
+        self.distance
+            + (other.fixed - self.start).abs()
+            + other.distance
+            + (self.fixed - other.start).abs()
+    }
+
+    fn contains(&self, point: i64) -> bool {
+        self.start.min(self.end) < point && point < self.start.max(self.end)
+    }
+
+    fn intersect(&self, other: &Line) -> Option<(i64, i64, i64)> {
+        if self.contains(other.fixed) && other.contains(self.fixed) {
+            Some((self.fixed, other.fixed, self.intersection_distance(other)))
+        } else {
+            None
+        }
+    }
+}
 
 fn parse_step(bytes: &[u8]) -> (u8, i64) {
     let mut steps = 0;
@@ -9,96 +36,81 @@ fn parse_step(bytes: &[u8]) -> (u8, i64) {
     (bytes[0], steps)
 }
 
-pub fn part1(input: &str) -> i64 {
-    let mut xs: BTreeMap<i64, Vec<(i64, i64)>> = BTreeMap::new();
-    let mut ys: BTreeMap<i64, Vec<(i64, i64)>> = BTreeMap::new();
-    let mut iter = input.lines();
+fn parse_line(line: &str) -> Vec<Vec<Line>> {
     let mut pos = (0, 0);
-    for value in iter.next().unwrap().trim().split(",") {
+    let mut distance = 0;
+    let mut lines = vec![vec![]; 2];
+    for value in line.trim().split(",") {
         let (dir, steps) = parse_step(value.trim().as_bytes());
-        match dir {
-            b'U' => {
-                xs.entry(pos.0).or_default().push((pos.1, pos.1 + steps));
-                pos = (pos.0, pos.1 + steps);
-            }
-
-            b'D' => {
-                xs.entry(pos.0).or_default().push((pos.1, pos.1 - steps));
-                pos = (pos.0, pos.1 - steps);
-            }
-            b'R' => {
-                ys.entry(pos.1).or_default().push((pos.0, pos.0 + steps));
-                pos = (pos.0 + steps, pos.1);
-            }
-            b'L' => {
-                ys.entry(pos.1).or_default().push((pos.0, pos.0 - steps));
-                pos = (pos.0 - steps, pos.1);
-            }
+        let (new_pos, horizontal) = match dir {
+            b'U' => ((pos.0, pos.1 + steps), false),
+            b'D' => ((pos.0, pos.1 - steps), false),
+            b'R' => ((pos.0 + steps, pos.1), true),
+            b'L' => ((pos.0 - steps, pos.1), true),
             _ => panic!(""),
-        }
-    }
-    let mut result = i64::MAX;
-    pos = (0, 0);
-    for value in iter.next().unwrap().trim().split(",") {
-        let (dir, steps) = parse_step(value.trim().as_bytes());
-        match dir {
-            b'U' => {
-                for (old_y, old_xs) in
-                    ys.range(pos.1.min(pos.1 + steps)..pos.1.max(pos.1 + steps) + 1)
-                {
-                    for old_x in old_xs {
-                        if old_x.0.min(old_x.1) < pos.0 && pos.0 < old_x.0.max(old_x.1) {
-                            result = result.min(old_y.abs() + pos.0.abs());
-                        }
-                    }
-                }
-                pos = (pos.0, pos.1 + steps);
+        };
+        let line = if horizontal {
+            Line {
+                fixed: pos.1,
+                start: pos.0,
+                end: new_pos.0,
+                distance: distance,
             }
+        } else {
+            Line {
+                fixed: pos.0,
+                start: pos.1,
+                end: new_pos.1,
+                distance: distance,
+            }
+        };
+        distance += (pos.0 - new_pos.0).abs() + (pos.1 - new_pos.1).abs();
+        pos = new_pos;
 
-            b'D' => {
-                for (old_y, old_xs) in
-                    ys.range(pos.1.min(pos.1 - steps)..pos.1.max(pos.1 - steps) + 1)
-                {
-                    for old_x in old_xs {
-                        if old_x.0.min(old_x.1) < pos.0 && pos.0 < old_x.0.max(old_x.1) {
-                            result = result.min(old_y.abs() + pos.0.abs());
-                        }
-                    }
-                }
-                pos = (pos.0, pos.1 - steps);
-            }
-            b'R' => {
-                for (old_x, old_ys) in
-                    xs.range(pos.0.min(pos.0 + steps)..pos.0.max(pos.0 + steps) + 1)
-                {
-                    for old_y in old_ys {
-                        if old_y.0.min(old_y.1) < pos.1 && pos.1 < old_y.0.max(old_y.1) {
-                            result = result.min(old_x.abs() + pos.1.abs());
-                        }
-                    }
-                }
-                pos = (pos.0 + steps, pos.1);
-            }
-            b'L' => {
-                for (old_x, old_ys) in
-                    xs.range(pos.0.min(pos.0 - steps)..pos.0.max(pos.0 - steps) + 1)
-                {
-                    for old_y in old_ys {
-                        if old_y.0.min(old_y.1) < pos.1 && pos.1 < old_y.0.max(old_y.1) {
-                            result = result.min(old_x.abs() + pos.1.abs());
-                        }
-                    }
-                }
-                pos = (pos.0 - steps, pos.1);
-            }
-            _ => panic!(""),
-        }
+        let index = if horizontal { 0 } else { 1 };
+        lines[index].push(line);
     }
-    result
+    lines
+}
+
+fn intersect_lines(hor_lines: &[Line], ver_lines: &[Line], length: bool) -> i64 {
+    hor_lines
+        .iter()
+        .filter_map(|hor_line| {
+            ver_lines
+                .iter()
+                .filter_map(|ver_line| ver_line.intersect(hor_line))
+                .map(|point| {
+                    if !length {
+                        point.0.abs() + point.1.abs()
+                    } else {
+                        point.2
+                    }
+                })
+                .min()
+        })
+        .min()
+        .unwrap_or(i64::MAX)
+}
+
+fn solve(input: &str, length: bool) -> i64 {
+    let mut iter = input.lines();
+    let first_lines = parse_line(iter.next().unwrap());
+    let second_lines = parse_line(iter.next().unwrap());
+
+    intersect_lines(&first_lines[0], &second_lines[1], length).min(intersect_lines(
+        &second_lines[0],
+        &first_lines[1],
+        length,
+    ))
+}
+
+pub fn part1(input: &str) -> i64 {
+    solve(input, false)
 }
 
 pub fn part2(input: &str) -> i64 {
-    0
+    solve(input, true)
 }
 
 #[cfg(test)]
@@ -123,6 +135,8 @@ mod tests {
 
     #[test]
     fn test_day3_part2() {
-        assert_eq!(part2(""), 0);
+        assert_eq!(part2(INPUT1), 30);
+        assert_eq!(part2(INPUT2), 610);
+        assert_eq!(part2(INPUT3), 410);
     }
 }
